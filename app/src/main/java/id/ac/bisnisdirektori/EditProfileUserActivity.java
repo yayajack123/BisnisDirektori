de8ac9cda07b62ce9a2bee341c1fb6370270ec3c
yayajack123 <juliartarya06@gmail.com>
2020-06-30 17:07:46 +0800

    update user profile dan halaman edit profile

---
+++ app/src/main/java/id/ac/bisnisdirektori/EditProfileUserActivity.java
@@ -0,0 +1,14 @@
+package id.ac.bisnisdirektori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfileUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_user);
+    }
+}
