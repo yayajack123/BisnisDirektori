150edad2cdf96e886fbb2ee3f3dc5f299f972047
krisna bayu <kbayu25@gmail.com>
2020-08-11 21:56:23 +0800

    Display list call user from database

---
+++ app/src/main/java/id/ac/bisnisdirektori/admin/adapterListDetailCall.java
@@ -0,0 +1,69 @@
+package id.ac.bisnisdirektori.admin;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import id.ac.bisnisdirektori.R;
+
+public class adapterListDetailCall extends BaseAdapter {
+
+    public static String ADMIN_PANEL_URL = "https://www.pantaucovid19.net/";
+    private Activity activity;
+    public adapterListDetailCall(Activity act) {
+        this.activity = act;
+    }
+
+
+    public int getCount() {
+        return ListCallActivity.id_call_user.size();
+    }
+    public Object getItem(int position) {
+        return position;
+    }
+    public long getItemId(int position) {
+        return position;
+    }
+    static class ViewHolder {
+        TextView txtIdData, txtFullname, txtPhonenumber, txtAddress, txtTime ;
+        ImageView imgThumb;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        adapterListDetailCall.ViewHolder holder;
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.row_list_call, null);
+            holder = new adapterListDetailCall.ViewHolder ();
+            convertView.setTag(holder);
+        }else{
+            holder = (adapterListDetailCall.ViewHolder) convertView.getTag();
+        }
+
+        holder.txtIdData = (TextView) convertView.findViewById(R.id.idData);
+        holder.txtFullname = (TextView) convertView.findViewById (R.id.txtNama);
+        holder.txtPhonenumber = (TextView) convertView.findViewById (R.id.txtPhonenumber);
+        holder.txtAddress = (TextView) convertView.findViewById (R.id.txtAddress);
+        holder.txtTime = (TextView) convertView.findViewById (R.id.textTime);
+        holder.imgThumb = (ImageView) convertView.findViewById(R.id.imgThumb);
+
+        holder.txtFullname.setText (ListCallActivity.fullname.get(position));
+        holder.txtPhonenumber.setText (ListCallActivity.phonenumber.get(position));
+        holder.txtAddress.setText (ListCallActivity.address.get(position));
+        holder.txtTime.setText (ListCallActivity.time.get(position));
+        holder.txtIdData.setText(ListCallActivity.id_data.get(position));
+        Picasso.with(activity).load(ADMIN_PANEL_URL+"/"+ListCallActivity.foto.get(position)).placeholder(R.drawable.imgthumb).into(holder.imgThumb);
+        return convertView;
+
+
+    }
+
+
+}
