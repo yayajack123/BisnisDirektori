b5b18ff71da19c206b2a9782e76678eb0e1df562
Ananda Dwi <anandadwi45@gmail.com>
2020-09-13 17:20:45 +0800

    Post Review

--- app/src/main/java/id/ac/bisnisdirektori/review/PostReviewActivity.java
+++ app/src/main/java/id/ac/bisnisdirektori/review/PostReviewActivity.java
@@ -2,22 +2,52 @@ package id.ac.bisnisdirektori.review;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import id.ac.bisnisdirektori.LoginActivity;
 import id.ac.bisnisdirektori.R;
+import id.ac.bisnisdirektori.admin.AppController;
 
 public class PostReviewActivity extends AppCompatActivity {
 
@@ -27,16 +57,28 @@ public class PostReviewActivity extends AppCompatActivity {
     String id;
     int IOConnect = 0;
     private String id_data, nama_bisnis;
-
+    int success;
+    private static final String TAG = PostReviewActivity.class.getSimpleName();
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    private String KEY_DATA = "id_data";
+    private String KEY_USER = "id_user";
+    private String KEY_RATE = "rate";
+    private String KEY_FOTO = "foto";
+    private String KEY_REVIEW = "review";
+    private String UPLOAD_URL;
     private RequestQueue requestQueue;
     private StringRequest stringRequest;
     private android.widget.RatingBar RatingBar;
     private TextView rate;
-
+    private int GALLERY = 1, CAMERA = 2;
+    Bitmap bitmap, decoded;
     EditText rating, foto, review;
-    Button submit, viewList;
+    Button submit, viewList, addPhoto;
+    ImageView reviewPhoto;
     String idd, idu, rat, fot, rev;
     public static final String Image = "image";
+    String tag_json_obj = "json_obj_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +87,12 @@ public class PostReviewActivity extends AppCompatActivity {
 
         sharedpreferences = getSharedPreferences(LoginActivity.my_shared_preferences, Context.MODE_PRIVATE);
 
-        id = sharedpreferences.getString("id_user", "null");
+        id = sharedpreferences.getString("id", "null");
 
         Intent iGet = getIntent();
         id_data = iGet.getStringExtra("id_data");
         nama_bisnis = iGet.getStringExtra("nama_bisnis");
-
+        UPLOAD_URL = "https://www.pantaucovid19.net/post_review.php";
         TextView title = (TextView) findViewById(R.id.title_product);
         title.setText(nama_bisnis);
 
@@ -62,40 +104,207 @@ public class PostReviewActivity extends AppCompatActivity {
                 rate.setText(String.valueOf(nilai));
             }
         });
+        review = findViewById (R.id.add_review);
+        addPhoto = findViewById (R.id.add_photos);
+        reviewPhoto = findViewById(R.id.photo_rvw);
+        submit = findViewById(R.id.submit_btn);
+
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureDialog();
+            }
+        });
 
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rat = rate.getText().toString();
+                rev = review.getText().toString();
 
-//        submit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                rat = rate.getText().toString();
-//                fot = foto.getText().toString();
-//                rev = review.getText().toString();
-//
-//                final DataBuku data = new DataBuku(
-////                        jud, gen, sin, Integer.parseInt(sto), Integer.parseInt(hrg), Integer.parseInt(pen)
-//                        id_data, id, sin, Integer.parseInt(hrg), Integer.parseInt(pen)
-//                );
-//                AppExecutors.getInstance().diskIO().execute(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                mDb.dataBukuDao().insertBuku(data);
-//                            }
-//                        });
-//                    }
-//                });
-//                uploadCRUD();
-//                startActivity(new Intent(AdminTambahBukuActivity.this, AdminBukuActivity.class));
-//            }
-//        });
-//        ivCamera.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SelectImageFromGallery();
-//            }
-//        });
+                uploadCRUD();
+
+//                startActivity(new Intent (InformationActivity.this, ListInformationActivity.class));
+//                finish ();
+
+            }
+        });
+
+    }
+
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Select Image");
+        String[] pictureDialogItems = {
+                "From gallery",
+                "From camera"};
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallery();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    public void choosePhotoFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    public String getStringImage(Bitmap bmp) {
+        if (bmp != null)
+        {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream ();
+            bmp.compress (Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] imageBytes = baos.toByteArray ();
+            String encodedImage = Base64.encodeToString (imageBytes, Base64.DEFAULT);
+            return encodedImage;
+        }else {
+            bmp = ((BitmapDrawable) reviewPhoto.getDrawable ()).getBitmap ();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream ();
+            bmp.compress (Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] imageBytes = baos.toByteArray ();
+            String encodedImage = Base64.encodeToString (imageBytes, Base64.DEFAULT);
+            return encodedImage;
+        }
+    }
+
+    private void setToImageView(Bitmap bmp) {
+        //compress image
+        if (bmp != null)
+        {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream ();
+            bmp.compress (Bitmap.CompressFormat.JPEG, 100, bytes);
+            decoded = BitmapFactory.decodeStream (new ByteArrayInputStream(bytes.toByteArray ()));
+            reviewPhoto.setImageBitmap (decoded);
+        }else{
+            bmp = ((BitmapDrawable) reviewPhoto.getDrawable ()).getBitmap ();
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream ();
+            bmp.compress (Bitmap.CompressFormat.JPEG, 100, bytes);
+            decoded = BitmapFactory.decodeStream (new ByteArrayInputStream (bytes.toByteArray ()));
+            reviewPhoto.setImageBitmap (decoded);
+        }
+
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, false);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                setToImageView(getResizedBitmap(bitmap, 512));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (requestCode == CAMERA && resultCode == RESULT_OK) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            File destination = new File(Environment.getExternalStorageDirectory(),
+                    System.currentTimeMillis() + ".jpg");
+            FileOutputStream fo;
+            try {
+                destination.createNewFile();
+                fo = new FileOutputStream(destination);
+                fo.write(bytes.toByteArray());
+                fo.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            decoded = BitmapFactory.decodeStream(new ByteArrayInputStream(bytes.toByteArray()));
+            reviewPhoto.setImageBitmap(decoded);
+            //Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            //setToImageView(getResizedBitmap(bitmap,1080));
+            // imgView.setImageURI(image_uri);
+        }
+    }
+
+    private void uploadCRUD() {
+        //menampilkan progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this, "Uploading...", "Please wait...", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e(TAG, "Response: " + response.toString());
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            success = jObj.getInt(TAG_SUCCESS);
+                            if (success == 1) {
+                                Log.e("v Add", jObj.toString());
+                                Toast.makeText(PostReviewActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(getIntent());
+                            } else {
+                                Toast.makeText(PostReviewActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        //menghilangkan progress dialog
+                        loading.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //menghilangkan progress dialog
+                        loading.dismiss();
+                        //menampilkan toast
+                        Toast.makeText(PostReviewActivity.this, "Please Complete Form & Image".toString(), Toast.LENGTH_LONG).show();
+                        Log.e(TAG, error.getMessage());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                //membuat parameters
+                Map<String, String> params = new HashMap<String, String>();
+                //menambah parameter yang di kirim ke web servis
+                params.put(KEY_DATA, id_data);
+                params.put(KEY_USER, id);
+                params.put(KEY_RATE, rat);
+                params.put(KEY_FOTO, getStringImage(decoded));
+                params.put(KEY_REVIEW, rev);
+                //kembali ke parameters
+                Log.e(TAG, "" + params);
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(stringRequest, tag_json_obj);
     }
 }
